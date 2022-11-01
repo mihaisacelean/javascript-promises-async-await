57fa53f436efcf9f64fca334e0c5e1fbea2c05f6
mihaisacelean <mihaisacelean@gmail.com>
2022-11-01 14:33:34 +0200

    -hjhjk

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+import fetchWithTimeOut from '../src/services';
+import * as movies from '../src/data/movies.json';
+
 export function fetchMovies() {
-    
+    const resolveFunction = () => movies;
+    return fetchWithTimeOut(1000).then(resolveFunction);
 }
+
+var moviePromise = fetchMovies();
+moviePromise.then();
