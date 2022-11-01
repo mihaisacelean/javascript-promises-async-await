3809a130f33ea4a832cee5fb925118b22b69ba29
mihaisacelean <mihaisacelean@gmail.com>
2022-11-01 15:29:48 +0200

    - module 4

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import { fetchWithTimeout } from './services';
 const movies = require('./data/movies.json');
 
-export function fetchMovies() {
-    const resolveFunction = () => movies;
-    return fetchWithTimeout(1000).then(resolveFunction);
+function getBooksAndMovies() {
+    return Promise.all([fetchBooks(), fetchMovies()]).then((
+        [books, movies]
+    ) => ({
+        books,
+        movies
+    })).catch(error => console.log("Error fetching books and movies", error));
 }
 
-const moviePromise = fetchMovies();
-moviePromise.then(results => {
-    console.log(results);
+function getBooksOrMovies() {
+    return Promise.race([fetchBooks(), fetchMovies()]).then(
+        results => results)
+    .catch(error => console.log("Error waiting for the promise race", error));
+}
+
+const getBooksAndMoviesPromise = getBooksAndMovies();
+getBooksAndMoviesPromise.then(results => {
+    console.log('getBooksAndMoviesPromise', results);
 });
+
